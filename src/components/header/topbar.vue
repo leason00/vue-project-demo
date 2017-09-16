c4a5ce9640f5e963c74dc3919634f35294c68f93
leason00 <22647085@qq.com>
2017-09-16 12:47:07 +0800

    [U]去除没用的东西

--- src/components/header/topbar.vue
+++ src/components/header/topbar.vue
@@ -5,44 +5,7 @@
 <template>
   <div class="cp-topbar clearfix">
     <div class="container clearfix">
-      <ul class="float-left" v-if="!hasLogin">
-        <li class="login"><router-link to="/login">欢迎，请登录</router-link></li>
-        <li><router-link to="/register">免费注册</a></router-link></li>
-      </ul>
-      <ul class="float-left" v-if="hasLogin">
-        欢迎您，<li><router-link :to="'/account/'+username+'/index'">{{username}}</router-link></li>
-        <li><a class="logout" @click="logout">退出登录</a></li>
-      </ul>
-      <ul class="float-right">
-        <li><router-link :to="'/account/'+username+'/index'">账户中心</router-link></li>
-        <li><router-link :to="'/account/'+username+'/order'">我的订单</router-link></li>
-        <li><router-link :to="'/account/'+username+'/collection'">我的收藏</router-link></li>
-        <li><router-link to="/join">商户入驻</router-link></li>
-        <li class="hasSubmenu">
-          <a href="javascript:void(0)">客户服务</a>
-          <div class="submenu service">
-            <div class="block block-top">
-              <p class="title">客户</p>
-              <ul>
-                <li v-for="item in service" :class="item.index%2===0?'li-right':'li-left'" v-if="item.cate === 'user'">
-                  <router-link to="/help">{{item.title}}</router-link>
-                </li>
-              </ul>
-            </div>
-            <div class="block">
-              <p class="title">商户</p>
-              <ul>
-                <li v-for="item in service" :class="item.index%2===0?'li-right':'li-left'" v-if="item.cate === 'seller'">
-                  <router-link to="/help">{{item.title}}</router-link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </li>
-        <li>
-          <a href="http://www.365greenlife.com/index" target="_blank">中绿生活网</a>
-        </li>
-      </ul>
+      <h1>test</h1>
     </div>
   </div>
 </template>
@@ -54,123 +17,20 @@
   export default{
     data() {
       return {
-        service:[
-          {index:1,cate:'user',title:'购物流程'},
-          {index:2,cate:'user',title:'支付流程'},
-          {index:3,cate:'user',title:'配送方式'},
-          {index:4,cate:'user',title:'售后政策'},
-          {index:5,cate:'user',title:'联系客服'},
-          {index:6,cate:'user',title:'意见反馈'},
-          {index:7,cate:'seller',title:'商家中心'},
-          {index:8,cate:'seller',title:'商家成长'},
-          {index:9,cate:'seller',title:'商家入驻'},
-          {index:10,cate:'seller',title:'商家品控'},
-          {index:11,cate:'seller',title:'商家规则'},
-          {index:12,cate:'seller',title:'商家工具'}
-        ],
-        hasLogin:false,
-        username:'请登录'
+
       }
     },
     created() {
-      let ssid = localStorage.getItem('ssid');
-      let username = localStorage.getItem('username');
-      if(ssid && username){
-        this.hasLogin = true;
-        this.username = username;
-      } else{
-        this.hasLogin = false;
-        this.username = null;
-        localStorage.removeItem('ssid');
-        localStorage.removeItem('username');
-      }
+
     },
     methods:{
-      logout:function () {
-        var that = this;
-        let params = {
-          url:COMMON.action.logout,
-          data:{}
-        };
-        api.post(params).then(function (response){
-          if(response.code === ERR_OK){
-            that.hasLogin = false;
-            that.username = null;
-            localStorage.removeItem('ssid');
-            localStorage.removeItem('username');
-            //广播
-            COMMON.hub.$emit('logout', true);
-            that.$router.push('/index');
-          }
-        })
-      }
+
     }
   }
 </script>
 
 <style lang="scss" rel='stylesheet/scss' scoped>
   @import 'src/assets/scss/style.scss';
-  /*一级菜单*/
-  .cp-topbar{
-    width:100%;
-    background: #f1f1f1;
-    .logout{
-      color: $error;
-      &:hover{
-        text-decoration: underline;
-      }
-    }
-  }
-  .container > ul > li{
-    display: inline-block;
-    position: relative;
-    &:hover  i{
-      transform:rotate(180deg);
-    }
-    &.hasSubmenu:hover{
-      background: #fff;
-      .submenu{
-        display: block;
-      }
-    }
-    & > a{
-      padding:0 10px;
-      height:32px;
-      line-height:32px;
-    }
-  }
-  a{
-    display: block;
-  }
-  /*二级菜单*/
-  .submenu{
-    display: none;
-    position: absolute;
-    top:32px;
-    right:0;
-    padding:10px 10px 0;
-    background: #fff;
-    z-index:999;
-    box-shadow: 1px 2px 1px rgba(0,0,0,.1),-1px 1px 1px rgba(0,0,0,.1);
-    .block-top{
-      margin-bottom:15px;
-    }
-    .title{
-      margin-bottom:5px;
-      font-weight:700;
-    }
-    a{
-      margin-bottom:10px;
-    }
-  }
-  .service ul{
-    width:120px;
-    li{
-      display: inline-block;
-      width:50px;
-      &.li-left{
-        margin-right:20px;
-      }
-    }
-  }
+
+
 </style>
--- src/router/router.js
+++ src/router/router.js
@@ -3,82 +3,11 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 // 首页相关
 const index = resolve => {require(['pages/index/index.vue'], resolve)}
-// const cooperation = resolve => {require(['pages/index/cooperation.vue'], resolve)}
-// const control = resolve => {require(['pages/index/control.vue'], resolve)}
-//
-// // 登录相关
-// const login = resolve => {require(['pages/login/login.vue'], resolve)}
-// const register = resolve => {require(['pages/login/register.vue'], resolve)}
-// const join = resolve => {require(['pages/login/join.vue'], resolve)}
-// const cpregister = resolve => {require(['pages/login/cpregister.vue'], resolve)}
-// const forgetpsw = resolve => {require(['pages/login/forgetPsw.vue'], resolve)}
-//
-//
-// //新闻相关
-// const news_list = resolve => {require(['pages/news/list.vue'], resolve)}
-// const news_detail = resolve => {require(['pages/news/detail.vue'], resolve)}
-// const news_about = resolve => {require(['pages/news/about.vue'], resolve)}
-// const nlist = resolve => {require(['pages/news/children/list.vue'], resolve)}
-// const ndetail = resolve => {require(['pages/news/children/detail.vue'], resolve)}
-//
-// // 店铺产品相关
-// const shop = resolve => {require(['pages/shop/index.vue'], resolve)}
-// const pro_detail = resolve => {require(['pages/product/detail.vue'], resolve)}
-// const search = resolve => {require(['pages/product/search.vue'], resolve)}
-// const shopcar = resolve => {require(['pages/product/shopcar.vue'], resolve)}
-// const pay = resolve => {require(['pages/product/pay.vue'], resolve)}
-//
-// // 个人中心
-// const account = resolve => {require(['pages/account/account.vue'], resolve)}
-// const pindex = resolve => {require(['pages/account/children/index.vue'], resolve)}
-// const porder = resolve => {require(['pages/account/children/order.vue'], resolve)}
-// const pcollection = resolve => {require(['pages/account/children/collection.vue'], resolve)}
-
-// 帮助中心
-// const help = resolve => {require(['pages/help/help.vue'], resolve)}
 
 const routes = [
   { path: '', component: index },
   { path: '/index', component: index },
-  // { path: '/cooperation', component: cooperation },
-  // { path: '/control', component: control },
-  // { path: '/login', component: login },
-  // { path: '/register', component: register },
-  // { path: '/join', component: join },
-  // { path: '/cpregister', component: cpregister },
-  // { path: '/forgetpsw', component: forgetpsw },
-  // { path: '/news/:newsType',name:'newsList', component: news_list },
-  // { path:'/about',name:'newsAbout',component:news_about,
-  //   children: [
-  //     { path: '', component: nlist },
-  //     { path: 'list/:listType', component: nlist },
-  //     { path: 'detail/:newsId', component: ndetail },
-  //   ]
-  // },
-  // { path: '/newsDetail/:nid', name:'newsDetail',component: news_detail },
-  // { path: '/shop/:sid', name:'shop',component: shop },
-  // { path: '/detail/:did', name:'detail',component: pro_detail },
-  // { path: '/search', component: search },
-  // { path: '/shopcar', component: shopcar },
-  // {
-  //   path: '/pay',
-  //   component: pay,
-  //   meta:{
-  //     'requireAuth':true
-  //   }
-  // },
-  // { path: '/account/:accountId', name:'account', component: account,
-  //     meta:{
-  //       'requireAuth':true
-  //     },
-  //     children: [
-  //       { path: '', component: pindex },
-  //       { path: 'index', component: pindex },
-  //       { path: 'order', component: porder },
-  //       { path: 'collection', component: pcollection }
-  //     ]
-  // },
-  // { path: '/help', component: help }
+  
 ]
 
 const router = new VueRouter({
